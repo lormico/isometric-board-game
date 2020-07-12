ab9c458be2b2799c25c4058b947bf5b62335ee11
Lorenzo <lor.mico@gmail.com>
2020-07-12 13:48:38 +0200

    Added pathfinding scripts, duplicated original tilemap

---
+++ Assets/Scripts/GameZone.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Game
+{
+    public class GameZone : MonoBehaviour
+    {
+        Tilemap boardTilemap;
+        Tilemap overlayTilemap;
+        PathFinder pathFinder;
+        List<Vector3Int> reachableCells;
+        bool mousePressed = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetupTiles();
+            pathFinder = new PathFinder(boardTilemap);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3Int cellCoords = GetCellUnderMouse();
+                if (pathFinder.IsCellWalkable(cellCoords))
+                {
+                    mousePressed = true;
+                    reachableCells = pathFinder.GetReachableCells(cellCoords, 4);
+                    foreach (Vector3Int cell in reachableCells)
+                    {
+                        overlayTilemap.SetTile(cell, Tiles.GREEN);
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (mousePressed)
+                {
+                    mousePressed = false;
+                    foreach (Vector3Int cell in reachableCells)
+                    {
+                        overlayTilemap.SetTile(cell, null);
+                    }
+
+                }
+            }
+            return;
+        }
+
+        private Vector3Int GetCellUnderMouse()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellCoords = boardTilemap.WorldToCell(mousePosition);
+            cellCoords.z = 0;
+            return cellCoords;
+        }
+
+        private void SetupTiles()
+        {
+            Tilemap[] tilemaps = GetComponentsInChildren<Tilemap>();
+            foreach (Tilemap tilemap in tilemaps)
+            {
+                if (tilemap.gameObject.name == "BoardTilemap")
+                {
+                    boardTilemap = tilemap;
+                }
+
+                if (tilemap.gameObject.name == "OverlayTilemap")
+                {
+                    overlayTilemap = tilemap;
+                }
+            }
+        }
+    }
+}
